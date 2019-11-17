c8052e87c3963c854333399a982fc028aa8886dd
kotvasili <kot_vasili@mail.ru>
2019-11-18 01:43:43 +0300

    eject, remove hashes

---
+++ precache-manifest.7db65c0906b491328f431fc28da31f17.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536d50d53edcbc6f680c0aaad071d3de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52e62eb5100fe0ba0219",
+    "url": "/static/css/main.chunk.css"
+  },
+  {
+    "revision": "32c29d23f1802bf69fdd",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "52e62eb5100fe0ba0219",
+    "url": "/static/js/main.chunk.js"
+  },
+  {
+    "revision": "06b335ca9524f5accb42",
+    "url": "/static/js/runtime-main.js"
+  },
+  {
+    "revision": "bd87c0ef7a0c09f5d6728a828bad61af",
+    "url": "/static/media/Cliker.svg"
+  },
+  {
+    "revision": "e0c46c0f04e10f02f2c0a4e995e38898",
+    "url": "/static/media/Close.svg"
+  },
+  {
+    "revision": "9659c3c2e5295774f6df7260216f4965",
+    "url": "/static/media/Cuff.svg"
+  },
+  {
+    "revision": "f9b18031b7ac1a61435f1bd687813bf2",
+    "url": "/static/media/Labret.svg"
+  },
+  {
+    "revision": "5f2f5d3a7cd0227b5bce7bf1bf3b9154",
+    "url": "/static/media/LeftEar.svg"
+  },
+  {
+    "revision": "c51da2b7fa4a38e2703b77077ae5f803",
+    "url": "/static/media/Plus.svg"
+  },
+  {
+    "revision": "3eea58194bb74a43aa6ea96021e6c097",
+    "url": "/static/media/Tick.svg"
+  },
+  {
+    "revision": "458aa564b9dd2b5d25c0d4fb01adee86",
+    "url": "/static/media/ear.jpg"
+  }
+]);
