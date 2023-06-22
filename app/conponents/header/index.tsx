9544371a14cd4cf17b200424448d196efafa3db4
vasilisaladar <v.saladar@sdventures.com>
2023-06-22 17:01:11 +0200

    fix

--- app/conponents/header/index.tsx
+++ app/conponents/header/index.tsx
@@ -1,5 +1,5 @@
 import styles from './header.module.sass'
-import Logo from 'public/assets/SiteLogo.svg';
+import Logo from 'public/assets/Sitelogo.svg';
 export const Header = () => {
     return <header className={styles.header}>
         <p>EVA AI SPECIAL</p>
