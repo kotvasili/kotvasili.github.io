a67dc71d19743d7a8dadb1d8eaa40979f31c86ee
vasilisaladar <v.saladar@sdventures.com>
2023-06-27 10:15:25 +0200

    fixes 9

--- app/conponents/StoryGrid/StoryGridItem/index.tsx
+++ app/conponents/StoryGrid/StoryGridItem/index.tsx
@@ -2,17 +2,17 @@
 import {IStoryCardFields} from "@/contentful/generated/types";
 import styles from './StoryGridItem.module.sass'
 import {CustomImage} from "@/app/conponents/CustomImage";
-import {useInView} from 'react-intersection-observer'
-import 'intersection-observer';
+import {useInView} from "framer-motion";
+import {useRef} from "react";
 
 export const StoryGridItem = (fields: IStoryCardFields) => {
-    // const ref = useRef(null)
-    const {ref, inView} = useInView( {
-        rootMargin: "0px 50px 0px",
-        threshold: 0.9
+    const ref = useRef(null)
+    const isInView = useInView(ref, {
+        margin: "0px 50px 0px",
+        amount: 0.9
     })
     return <div className={styles.grid_card}>
-        <div className={`${styles.story} ${inView ? styles.story_active: ''}`} style={{maxWidth: fields.image.fields.file.details.image?.width}}>
+        <div className={`${styles.story} ${isInView ? styles.story_active: ''}`} style={{maxWidth: fields.image.fields.file.details.image?.width}}>
             <span>story #{fields.number}</span>
             <div className={styles.story_back}  ref={ref}>
                 <CustomImage {...fields.image}/>
