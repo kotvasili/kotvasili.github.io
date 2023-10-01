cbcdb7f2e5fefc6162c88f65ce5ec523a88486e9
vasilisaladar <v.saladar@sdventures.com>
2023-10-01 22:58:00 +0300

    eva 5

--- components/AvatarImage/index.tsx
+++ components/AvatarImage/index.tsx
@@ -1,48 +1,23 @@
 'use client'
-import {FC, useState} from "react";
-import { motion} from "framer-motion";
 import Image from "next/image";
 import {Asset} from "contentful";
+import styles from './AvatarImage.module.sass'
 
-const variants = {
-    initial: {
-        opacity: 0,
-        filter: "blur(15px)",
-    },
-    animate: {
-        opacity: 1,
-        filter: "blur(0px)",
-        transition: { type: "spring", duration: 1, delay: 0.5 }
-    }
-}
 export const AvatarImage = ({fields: {file, title}, className = '', delay, single = false}: Asset & {className?: string; delay: number; single?: boolean}) => {
-    const [loaded, setLoaded] = useState(false)
-    const vars = {
-        ...variants,
-        animate: {
-            ...variants.animate,
-            transition: {
-                ...variants.animate.transition,
-                delay: delay ? variants.animate.transition.delay + delay : variants.animate.transition.delay
-            }
-        }
-    }
-    return <motion.div
-            key={+loaded}
+    return <div
             className={className}
-            variants={vars}
-            animate="animate"
-            initial="initial"
         >
             <Image
                 alt={title}
                 src={file.url ? `https:${file.url}?fm=webp&q=100` : ''}
                 fill
+                style={{transitionDelay: delay ? `${0.5 + delay}s` : '0.5s' }}
                 decoding='async'
+                className={styles.avaimg}
                 sizes={single ? "(max-width: 600px) 70vw, (max-width: 1000px) 50vw, 70vw" : "(max-width: 600px) 66vw, (max-width: 1000px) 33vw, 25vw"}
-                onLoadingComplete={() => setLoaded(true)}
+                onLoadingComplete={img => img.classList.add(styles.avaimg_load)}
                 quality={100}
                 priority={single}
             />
-        </motion.div>
+        </div>
 }
